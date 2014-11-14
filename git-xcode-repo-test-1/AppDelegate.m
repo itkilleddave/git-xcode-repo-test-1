92852952c9a8c76a0f4c1e74c4416d659ca5439a
Dave <Dave@Daves-iMac-2.local>
2014-11-14 14:28:43 +1030

    my first commit - simple comment for testing purposes

--- git-xcode-repo-test-1/AppDelegate.m
+++ git-xcode-repo-test-1/AppDelegate.m
@@ -17,6 +17,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    //my note to test commit
+    
     return YES;
 }
 
